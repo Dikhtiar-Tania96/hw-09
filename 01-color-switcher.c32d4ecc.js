7ff280ff3a88f130f8512f24aa00a503a08ddcd5
Dikhtiar-Tania96 <103426588+Dikhtiar-Tania96@users.noreply.github.com>
2024-04-15 11:13:01 +0000

    Deploying to gh-pages from @ Dikhtiar-Tania96/hw-09@c1bd32c81eab834a8ab2d71746e00a01e7315d9c 🚀

---
+++ 01-color-switcher.c32d4ecc.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.disable=!0}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.disable=!1,тn}));var o=null}();
+//# sourceMappingURL=01-color-switcher.c32d4ecc.js.map
